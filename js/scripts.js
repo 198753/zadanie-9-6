e5afb8fbd209c2ce1f2bd807f2c83ba446c9d9c5
Tomasz Andrzejewski <kasta.tomek@wp.pl>
2017-10-04 09:30:18 +0200

    o to chodziło?

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+var i = document.getElementsByTagName('li');
+
+add.addEventListener('click', function() {
+	var element = document.createElement('li');
+	
+	element.innerHTML = 'item' + i.length;
+	list.appendChild(element);	
+});
